b13ee4b57301fc73ac18f569cb9c8188d2ca5e26
Ashish Srivastava <ashish.sri101@gmail.com>
2023-11-02 01:13:32 +0530

    Add files via upload

--- home.js
+++ home.js
@@ -1,20 +1,23 @@
 function submitForm() {
 
+
     const options = {
         method: 'POST',
+        mode: 'no-cors',
         headers: {
           accept: 'application/json',
           'content-type': 'application/*+json',
           Authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJteHBueWxzbmt5bTV4c3k3ZGttNyIsIm14LXdzaWQiOiIzM0MyQ0I2OS00MEZELTREQTItQjA1OS1BQjUzNjkzN0I5RDciLCJleHAiOjE3OTMzMTg0MDB9.LGEBAXuZuyTVtPBziDPMvCcwt6x0iurwO3AXKC1tEbc'
         },
-        body: '{"status":0,"legacyContactKey":"SW5kaXZpZHVhbAkyMzEwMzEyNTIwMDAzOTA5NTEzNDBDCTA=","name":"test","startDate":"11-11-2012","description":"desc","cost":10000}'
+        body: '{"contactType":0,"companyName":'+document.getElementById("companyName").value+',"firstName":'+document.getElementById("firstName").value+',"lastName":'+document.getElementById("lastName").value+',"website":'+document.getElementById("website").value+',"phones":[{"number":'+document.getElementById("phone").value+'}],"emails": [{"address":'+document.getElementById("email").value+'}]}'
       };
       
-      fetch('https://api.maximizer.com/ferret/v1/opportunities', options)
+      fetch('https://api.maximizer.com/ferret/v1/contacts', options)
         .then(response => response.json())
         .then(response => console.log(response))
         .catch(err => console.error(err));
-        
+
+
     // var body = {
     //     "contactType":0,
     //     "companyName": document.getElementById("companyName").value,
@@ -28,7 +31,7 @@ function submitForm() {
     // var xhr = new XMLHttpRequest();
 
     // xhr.addEventListener("readystatechange", function () {
-    //     if (xhr1.status === 201) {
+    //     if (xhr.status === 201) {
     //         var data = JSON.parse(xhr.response);
 
     //         // 
